d66c9ff6a8978d23b8669f18078523a21e540613
program_with_abhi <programwithabhi@gmail.com>
2022-08-21 14:32:06 +0530

    First Commit

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,16 +1,27 @@
 <?php
     class users extends my_controller {
-        public function _construct(){
-            // parent::_construct();
-            echo "This is the users class constructor";
-        }
+        // When you define this class constructor you have to call the ci_controller constructor to acces the super object 
+        // public function __construct(){
+        //     // parent::__construct();
+        //     // echo "This is the users class constructor";
+        // }
 
         public function index(){
-            // $this->_construct();
+           
+            $this->__construct();
             echo "This is the index function of the user class";
             $this->load->helper('form');
             $this->load->view('users/articles');
         }
+
+        // public function abhi(){
+             // $this->load->library('cart');
+            // print_r($this->cart);
+        //     echo 'this is the abhi function inside the users controller';
+        // //   $abhi =  $this->cart->insert(["name"=>'abhishek']);
+        //   print_r($this->cart);
+
+        // }
     }
 
 ?>
--- application/core/my_controller.php
+++ application/core/my_controller.php
@@ -1,8 +1,11 @@
 <?php
     class my_controller extends CI_Controller {
-        public function _construct() {
-            // parent::__construct();
-           echo "This is the my_controller class constructor<br>";
+        // public function __construct() {
+        //     // parent::__construct();
+        //    echo "This is the my_controller class constructor<br>";
+        // }
+        public function toThing(){
+            echo "This is the toThing function under the my_controller";
         }
     }
 
